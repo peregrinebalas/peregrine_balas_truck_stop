a9e47792cb5c77570da820d0113bd1b3fb75831b
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-04-11 15:14:50 -0500

    update routes/

--- lib/food_truck_stop_web/router.ex
+++ lib/food_truck_stop_web/router.ex
@@ -10,39 +10,16 @@ defmodule FoodTruckStopWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  # TODO add authentication plug
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", FoodTruckStopWeb do
-    pipe_through :browser
-
-    get "/", PageController, :home
-  end
-
-  # Other scopes may use custom stacks.
+  # API endpoints for food trucks
   scope "/api", FoodTruckStopWeb do
     pipe_through :api
+    get "/foodtrucks/search", FoodTruckController, :search
 
-    get "/food_trucks", FoodTruckController, :index
-    get "/food_trucks/:food_truck", FoodTruckController, :show
-    get "/food_trucks/search", FoodTruckController, :search
-  end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:food_truck_stop, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: FoodTruckStopWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    resources "/foodtrucks", FoodTruckController, only: [:index, :show]
   end
 end
